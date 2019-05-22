7def5a28a72fc149453a28bcf7f27dc007029671
PrashantAlhat <prashant.alhat@gmail.com>
2019-05-22 12:07:15 +0530

    Added code for Autentication

--- app/src/main/java/com/kfandra/tzlc12/MainActivity.java
+++ app/src/main/java/com/kfandra/tzlc12/MainActivity.java
@@ -1,6 +1,8 @@
 package com.kfandra.tzlc12;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -8,8 +10,21 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final int RC_SIGN_IN = 1;
+
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,35 +33,80 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        //prashant.alhat@kfandra.com  : Test123$
+        //kfandraai@kfandra.com
+        //MRManager@kfandra.com
+        final Button clubs = findViewById(R.id.butClubs);
+        final Button players = findViewById(R.id.butPlayer);
+        final Button fixtures = findViewById(R.id.butFixtures);
+        final Button balancesheet = findViewById(R.id.butBalanceSheet);
+
+
+        authStateListener = new FirebaseAuth.AuthStateListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if(user != null){
+                    //signed in
+                    if (user.getEmail().equalsIgnoreCase("kfandraai@kfandra.com") || user.getEmail().contains("manager")){
+                        clubs.setVisibility(View.VISIBLE);
+                        players.setVisibility(View.VISIBLE);
+                        fixtures.setVisibility(View.VISIBLE);
+                        balancesheet.setVisibility(View.VISIBLE);
+                    }else {
+                        clubs.setVisibility(View.VISIBLE);
+                        players.setVisibility(View.VISIBLE);
+                        fixtures.setVisibility(View.VISIBLE);
+                        balancesheet.setVisibility(View.GONE);
+                    }
+                    //Toast.makeText(MainActivity.this, user.getDisplayName() + " Signed  IN", Toast.LENGTH_LONG).show();
+                }else{
+                    //signed out
+                    startActivityForResult(
+                            AuthUI.getInstance()
+                                    .createSignInIntentBuilder()
+                                    .setIsSmartLockEnabled(false)
+                                    .setAvailableProviders(Arrays.asList(
+                                            new AuthUI.IdpConfig.EmailBuilder().build(),
+                                            new AuthUI.IdpConfig.GoogleBuilder().build()
+                                    ))
+                                    .build(),RC_SIGN_IN
+                    );
+                }
+
             }
-        });
+        };
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (id)
+        {
+            case R.id.signout :
+                AuthUI.getInstance().signOut(this);
+                break;
         }
-
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        firebaseAuth.removeAuthStateListener(authStateListener);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        firebaseAuth.addAuthStateListener(authStateListener);
     }
 }
