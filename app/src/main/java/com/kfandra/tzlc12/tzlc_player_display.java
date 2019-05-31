ed0420b3b8e7a94be9836ae08b428b0287d72170
PrashantAlhat <prashant.alhat@gmail.com>
2019-05-31 13:34:39 +0530

    sorting added to player display

--- app/src/main/java/com/kfandra/tzlc12/tzlc_player_display.java
+++ app/src/main/java/com/kfandra/tzlc12/tzlc_player_display.java
@@ -1,5 +1,6 @@
 package com.kfandra.tzlc12;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,6 +8,8 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ListView;
 
@@ -17,6 +20,8 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class tzlc_player_display extends AppCompatActivity {
@@ -25,6 +30,8 @@ public class tzlc_player_display extends AppCompatActivity {
     private List<Player> players;
     ListView playerList;
     public int scrollIndex=0;
+    Query query;
+    FirebaseDatabase database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,8 +46,8 @@ public class tzlc_player_display extends AppCompatActivity {
         players = new ArrayList<Player>();
         playerList = findViewById(R.id.listPlayer);
 
-        FirebaseDatabase database = FirebaseDatabase.getInstance("https://tzlc12.firebaseio.com/");
-        Query query = database.getReference("/players").orderByChild("playerName");
+        database = FirebaseDatabase.getInstance("https://tzlc12.firebaseio.com/");
+        query = database.getReference("/players").orderByChild("playerName");
 
         query.addChildEventListener(new ChildEventListener() {
             @Override
@@ -74,6 +81,69 @@ public class tzlc_player_display extends AppCompatActivity {
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle(getString(R.string.app_name) + " Players");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_players,menu);
+
+        return true;
+        //return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        /*if(!role.equalsIgnoreCase("KFANDRAAI")){
+            menu.findItem(R.id.addFixture).setVisible(false);
+            menu.findItem(R.id.editFixture).setVisible(false);
+            menu.findItem(R.id.deleteFixture).setVisible(false);
+        }else{
+            menu.findItem(R.id.addFixture).setVisible(true);
+            menu.findItem(R.id.editFixture).setVisible(true);
+            menu.findItem(R.id.deleteFixture).setVisible(true);
+        }
+        return true;*/
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        adapterPlayer adapterPlayer;
+        switch (item.getItemId()){
+            case R.id.sortClub :
+                Collections.sort(players, new Comparator<Player>() {
+                    @Override
+                    public int compare(Player o1, Player o2) {
+                        return o1.getClubName().compareToIgnoreCase(o2.getClubName());
+                    }
+                });
+                adapterPlayer = new adapterPlayer(tzlc_player_display.this, R.layout.listitemplayer, players);
+                playerList.setAdapter(adapterPlayer);
+                break;
+            case R.id.sortPlayer :
+                Collections.sort(players, new Comparator<Player>() {
+                    @Override
+                    public int compare(Player o1, Player o2) {
+                        return o1.getPlayerName().compareToIgnoreCase(o2.getPlayerName());
+                    }
+                });
+                adapterPlayer = new adapterPlayer(tzlc_player_display.this, R.layout.listitemplayer, players);
+                playerList.setAdapter(adapterPlayer);
+                break;
+            case R.id.sortValue :
+                query = database.getReference("/players").orderByChild("currentValue");
+                Collections.sort(players, new Comparator<Player>() {
+                    @Override
+                    public int compare(Player o1, Player o2) {
+                        return (o1.getCurrentValue() > o2.getCurrentValue() ? -1 : o1.getCurrentValue() == o2.getCurrentValue() ? 0 : 1);
+                    }
+                });
+                adapterPlayer = new adapterPlayer(tzlc_player_display.this, R.layout.listitemplayer, players);
+                playerList.setAdapter(adapterPlayer);
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
 }
